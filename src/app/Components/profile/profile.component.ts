532e708fa7857ee8e4475a9be494282a02c46e9e
Arthur Van Loo <arthur@vanloo.biz>
2024-04-11 21:51:47 +0200

    Final Update

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule, matTabsAnimations } from '@angular/material/tabs';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, MatCardModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
