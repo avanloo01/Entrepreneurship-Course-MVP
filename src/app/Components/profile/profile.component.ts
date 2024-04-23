89ed4acafa764c03119f1f76ce2bc87f47bd4860
Arthur Van Loo <arthur@vanloo.biz>
2024-04-23 21:35:04 +0200

    Latest update

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule, matTabsAnimations } from '@angular/material/tabs';
+import { DateRange, MatCalendarCellClassFunction, MatCalendarCellCssClasses, MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatTabsModule, MatCardModule],
+  imports: [MatTabsModule, MatCardModule, MatDatepickerModule],
+  providers: provideNativeDateAdapter(),
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  selected: Date | null | undefined;
+  
 }
